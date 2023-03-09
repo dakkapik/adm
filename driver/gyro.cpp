3a7719e6be540572174f497a067e811919ddc9c5
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-03-09 16:11:33 -0500

    bell curve filter

--- driver/gyro.cpp
+++ driver/gyro.cpp
@@ -8,9 +8,14 @@
 #include <iostream>
 
 #include <chrono>
+#include <thread>
 #include <cmath>
 #include <math.h>
 
+uint16_t delayBefore = 1000;
+uint16_t delayAfter = 3000;
+
+float gyroXoffset, gyroYoffset, gyroZoffset;
 
 using namespace std::chrono;
 
@@ -52,10 +57,49 @@ short read_raw_data(int addr){
 	return value;
 }
 
-void ms_delay(int val){
-	int i,j;
-	for(i=0;i<=val;i++)
-		for(j=0;j<1200;j++);
+void setGyroOffsets (float x, float y, float z) {
+	gyroXoffset = x;
+  gyroYoffset = y;
+  gyroZoffset = z;
+}
+
+void calcOffsets(bool console){
+	float x = 0, y = 0, z = 0;
+	int16_t rx, ry, rz;
+
+  delay(delayBefore);
+	if(console){
+    Serial.println();
+    Serial.println("========================================");
+    Serial.println("Calculating gyro offsets");
+    Serial.print("DO NOT MOVE MPU6050");
+  }
+  for(int i = 0; i < 3000; i++){
+    if(console && i % 1000 == 0){
+      Serial.print(".");
+    }
+    rx = read_raw_data(GYRO_XOUT_H);
+    ry = read_raw_data(GYRO_YOUT_H);
+    rz = read_raw_data(GYRO_ZOUT_H);
+ 
+    x += ((float)rx) / 65.5;
+    y += ((float)ry) / 65.5;
+    z += ((float)rz) / 65.5;
+  }
+  gyroXoffset = x / 3000;
+  gyroYoffset = y / 3000;
+  gyroZoffset = z / 3000;
+
+  if(console){
+    Serial.println();
+    Serial.println("Done!");
+    Serial.print("X : ");Serial.println(gyroXoffset);
+    Serial.print("Y : ");Serial.println(gyroYoffset);
+    Serial.print("Z : ");Serial.println(gyroZoffset);
+    Serial.println("Program will start after 3 seconds");
+    Serial.print("========================================");
+		delay(delayAfter);
+	}
 }
 
 int main(){
@@ -67,7 +111,8 @@ int main(){
 	float elapsedTime, time, timePrev;
 	int i;
 	float rad_to_deg = 180/3.141592654;
-
+	
+	setGyroOffsets(1.45, 1.23, -1.32);
 
 	fd = wiringPiI2CSetup(Device_Address);   /*Initializes I2C with device Address*/
 	MPU6050_Init();		                 /* Initializes MPU6050 */
@@ -97,19 +142,19 @@ int main(){
 		Gyro_angle[0] = Gyr_rawX/131.0; 
 		/*---Y---*/
 		Gyro_angle[1] = Gyr_rawY/131.0;
-        	Gyro_angle[2] = Gyr_rawZ/131.0;
+        Gyro_angle[2] = Gyr_rawZ/131.0;
+        // Gyro_angle[2] = Gyr_rawZ/65.5;
 
+		Gyro_angle[0] -= gyroXoffset;
+		Gyro_angle[1] -= gyroYoffset;
+        Gyro_angle[2] -= gyroZoffset;
 
 		/*---X axis angle---*/
 		Total_angle[0] = 0.98 *(Total_angle[0] + Gyro_angle[0]*elapsedTime) + 0.02*Acceleration_angle[0];
 		/*---Y axis angle---*/
 		Total_angle[1] =  atan(-1*(Acc_rawX/16384.0)/sqrt(pow((Acc_rawY/16384.0),2) + pow((Acc_rawZ/16384.0),2)))*rad_to_deg;
 
-        // Total_angle[2] = Gyro_angle[2];
-        // Total_angle[2]= Acceleration_angle[2];
-        // Total_angle[2] = Acc_rawZ;
-        // Total_angle[2] =  0.98 *(Total_angle[2] + Gyro_angle[2]*elapsedTime);
-		Total_angle[2] =  atan(Gyro_angle[1]/(sqrt(pow(Gyro_angle[0],2)+pow(Gyro_angle[2],2))));
+		Total_angle[2] = Total_angle[2] + Gyro_angle[2]*elapsedTime;
 
 		printf("%f,%f,%f,%f,%f,%f\r", Total_angle[0], Total_angle[1], Total_angle[2], magnet[0], magnet[1], magnet[2]);
 
